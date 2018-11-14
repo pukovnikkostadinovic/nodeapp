e6cfceeec7f73a2edcc93f2ad8e64468c04f2572
pukovnikkostadinovic <pukovnikkostadinovic@gmail.com>
2018-11-14 18:43:44 +0100

    svasta

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log('Hello World');
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+
+app.get('/', function(req,res){
+  res.render('index',{
+    title:'title'
+  });
+});
+
+app.get('/articles/add', function(req,res){
+  res.render('add',{
+    title:'title'
+  });
+});
+
+
+app.listen(3000, function(){
+  console.log('Server started on port 3000...');
+});
