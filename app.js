3414ba2cfafcdbd2e3cab970018314f5665a92ce
pukovnikkostadinovic <pukovnikkostadinovic@gmail.com>
2018-11-14 19:29:35 +0100

    uspio dodavanje posta

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
 
 mongoose.connect('mongodb://localhost/nodeapp');
 let db = mongoose.connection;
@@ -20,6 +22,11 @@ let Article = require('./models/article');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 app.get('/', function(req,res){
   Article.find({}, function(err, articles){
@@ -40,6 +47,20 @@ app.get('/articles/add', function(req,res){
   });
 });
 
+app.post('/articles/add', function(req,res){
+  let article = new Article();
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body
+
+  article.save(function(err){
+    if(err){
+      console.log(err);
+    }else {
+      res.redirect('/');
+    }
+  });
+});
 
 app.listen(3000, function(){
   console.log('Server started on port 3000...');
