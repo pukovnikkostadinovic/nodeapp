58b0f844b0e89d8fa8409b7d7dfed9fc47eb5b2c
pukovnikkostadinovic <pukovnikkostadinovic@gmail.com>
2018-11-16 16:26:27 +0100

    dodavanje

--- routes/arduino.js
+++ routes/arduino.js
@@ -6,11 +6,42 @@ let connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'dlibreman',
-  database: 'beer'
+  database: 'beer',
+  multipleStatements: true
 });
 
 connection.connect();
 
+router.get('/dodaj', function(req,res){
+  let query = 'select id, ime_kategorije from kategorije_komponenti; select * from lokacije';
+
+  connection.query(query, [1,2], function(err, result){
+    //console.log(result[0]);
+    //console.log(result[1]);
+    res.render('dodaj_komp', {
+      kategorije:result[0],
+      lokacije:result[1]
+    });
+  });
+});
+
+router.post('/dodaj', function(req, res){
+  let query = "INSERT INTO `komponente` (`ime_komponente`, `kratak_opis_komp`, `kateg_id`) VALUES ('"+req.body.ime_komp+"','"+req.body.k_opis_komp+"',"+req.body.kateg+");";
+  let query2 =  "INSERT INTO `komp_lok_kol` (`komp_id`, `lok_id`, `kolicina`) select id, "+req.body.lok+","+req.body.kol+" from komponente where ime_komponente='"+req.body.ime_komp+"'";
+
+  //console.log(query);
+  connection.query(query+query2,[1,2], function(err, result){
+    if(err) throw err;
+    req.flash('success','Komponenta dodana');
+    res.redirect('/arduino/dodaj');
+  });
+  //console.log('ime_komponente je: '+req.body.ime_komp);
+  //console.log('Kratki opis je: '+req.body.k_opis_komp);
+  //onsole.log('kolicina je: '+req.body.kol);
+  //console.log('Kategorija je: '+req.body.kateg);
+  //console.log('lokacija je: '+req.body.lok);
+  //console.log(query2);
+});
 
 router.get('/sve', function(req, res){
   let query = 'select t.id, t.ime_komponente, t.kateg_id, sum(k.kolicina) kolicina from komponente t, komp_lok_kol k where t.id=k.komp_id group by t.id, t.ime_komponente, t.kateg_id';
