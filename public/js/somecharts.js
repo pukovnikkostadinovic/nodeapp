c5e08d7aee8f6e901e572bc98cfcf3ce339ff92c
pukovnikkostadinovic <pukovnikkostadinovic@gmail.com>
2018-11-30 19:32:42 +0100

    jhdakshdkahsdk

--- public/js/somecharts.js
+++ public/js/somecharts.js
@@ -1,8 +1,9 @@
 var socket = io.connect('http://localhost:3000');
 //var socket = io.connect('http://yeti.cf:80');
 
-var ctx = document.getElementById('chart').getContext('2d')
-    var ctx1 = document.getElementById('bar_chart').getContext('2d')
+var ctx = document.getElementById('line_chart_temp').getContext('2d')
+var ctx1 = document.getElementById('line_chart_humid').getContext('2d')
+var ctx2 = document.getElementById('bar_chart').getContext('2d')
     var hdata = {
       labels: [0],
       datasets: [{
@@ -20,6 +21,37 @@ var ctx = document.getElementById('chart').getContext('2d')
         borderColor:'#2c3e50'
       }]
     }
+		var temp_data = {
+      labels: [0],
+      datasets: [{
+        data: [0],
+        label: 'Temperature',
+        fill: false,
+        //backgroundColor: '#fdfefe',
+        borderColor:'#ff6600'
+      }]
+    };
+		var temp_options=  {
+			reponsive:true,
+      animation: {
+      duration: 1400,
+      easing:'linear',
+			scales:{
+				yAxes: [
+					{
+						display:true,
+						ticks:{
+							//beginAtZero:true,
+							min:10,
+							steps: 11,
+							stepValue: 1,
+							max:35
+						}
+					}
+				]
+			}
+    }
+	};
     var bar_data = {
       labels:["TEmp1","Temp2"],
       datasets: [
@@ -44,10 +76,10 @@ var ctx = document.getElementById('chart').getContext('2d')
     } }
     var chart = new Chart(ctx, {
       type: 'line',
-      data: hdata,
-      options: optionsAnimations
+      data: temp_data,
+      options: temp_options
     });
-    var bar_chart = new Chart(ctx1, {
+    var bar_chart = new Chart(ctx2, {
       type:'bar',
       data:bar_data,
       options:{
@@ -73,21 +105,21 @@ var ctx = document.getElementById('chart').getContext('2d')
       }
     });
     socket.on('temperature', function (value) {
-      var length = hdata.labels.length
+      var length = temp_data.labels.length
       if (length >= 30) {
-        hdata.datasets[0].data.shift()
-        hdata.datasets[1].data.shift()
-        hdata.labels.shift()
+        temp_data.datasets[0].data.shift()
+        //temp_data.datasets[1].data.shift()
+        temp_data.labels.shift()
       }
 
-      hdata.labels.push(moment().format('HH:mm:ss'))
-      hdata.datasets[0].data.push(value.temp1)
-      hdata.datasets[1].data.push(value.temp2)
+      temp_data.labels.push(moment().format('HH:mm:ss'))
+      temp_data.datasets[0].data.push(value.temp1)
+      //temp_data.datasets[1].data.push(value.temp2)
 
       bar_data.datasets[0].data.shift()
-      bar_data.datasets[1].data.shift()
+      //bar_data.datasets[1].data.shift()
       bar_data.datasets[0].data.push(value.temp1);
-      bar_data.datasets[1].data.push(value.temp2);
+      //bar_data.datasets[1].data.push(value.temp2);
       bar_chart.update(0);
       chart.update();
 
